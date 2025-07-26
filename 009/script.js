ace14dd4af60a49c1076e0bfea77ffc07a806d30
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-26 18:42:57 +0530

    update on JS

--- 009/script.js
+++ 009/script.js
@@ -1,1 +1,11 @@
 console.log("Reduce method");
+
+let prices = [2,45,33,200,50,43];
+
+const total = prices.reduce(getTotal);
+
+function getTotal(accumulator, element){
+    return accumulator+element;
+}
+
+console.log("Price total "+ total);
