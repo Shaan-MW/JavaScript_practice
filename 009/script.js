013988cd65a59e460678a7698e3c5529094acc29
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-26 19:12:55 +0530

    more on reduce

--- 009/script.js
+++ 009/script.js
@@ -9,3 +9,30 @@ function getTotal(accumulator, element){
 }
 
 console.log("Price total "+ total);
+
+let grades = [65,43,76,88,76,43,90];
+console.log("Shaan Marks - "+ grades);
+const totalMarks =grades.reduce(getTotal);
+console.log("Total : "+ totalMarks + "/"+grades.length*100);
+
+if(totalMarks>=400){
+    console.log("You are passed.");
+}else{
+    console.log("You are failed");
+}
+
+
+let randomNumbers = [10,20,43,32,5,78,54,90];
+const max = randomNumbers.reduce(getMax);
+function getMax(accumulator, element){
+    return Math.max(accumulator,element);
+}
+
+console.log("Calculated Maximum : " + max);
+
+const min = randomNumbers.reduce(getMin);
+console.log("Calculated Minimum : "+ min);
+
+function getMin(accumulator,element){
+    return Math.min(accumulator,element);
+}
