c02a8a09393b246d3d192f5816baeb09691396cd
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-27 16:24:37 +0530

    update script.js

--- 012/script.js
+++ 012/script.js
@@ -1,10 +1,27 @@
 console.log("Objects in Java Script.");
 
-const person = {
+const person1 = {
     firstName:"Shaan",
     secondName:"Madhiwanan",
     age:22,
-    isUngergraduate :true
+    isUngergraduate :true,
+    sayHello: function(){console.log(`Hello, I'm ${this.firstName}`);},
 };
 
-console.log(`hello!, ${person.firstName}`);
+const person2 = {
+    firstName:"Patrick",
+    secondName:"Bateman",
+    age:35,
+    isUngergraduate :false,
+    sayHello: function(){console.log(`Hello, I'm ${this.firstName}`);},
+};
+
+console.log(`hello!, ${person1.firstName}`);
+person1.sayHello();
+
+console.log(`hello!, ${person2.firstName}`);
+person2.sayHello();
+
+for(let key in person1){
+    console.log(person1[key]);
+}
