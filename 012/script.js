a7a35dd9da3f159e61a308d09ed5cf68b0c0ec48
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-27 15:48:22 +0530

    updates

---
+++ 012/script.js
@@ -0,0 +1,10 @@
+console.log("Objects in Java Script.");
+
+const person = {
+    firstName:"Shaan",
+    secondName:"Madhiwanan",
+    age:22,
+    isUngergraduate :true
+};
+
+console.log(`hello!, ${person.firstName}`);
