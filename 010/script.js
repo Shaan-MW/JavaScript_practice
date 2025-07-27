5606882cf1d74b869d795e72510093aa6d027a0f
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-27 13:17:08 +0530

    Function Expression 2

--- 010/script.js
+++ 010/script.js
@@ -9,4 +9,58 @@ console.log(greet("shaan"));
 
 setTimeout(function(){
     console.log("hello (delay 3 sec)");
-} , 3000);
+} , 1000);
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+
+let squares = numbers.map(function(element){
+    return Math.pow(element,2);
+});
+console.log(squares);
+
+let cubes = numbers.map(function(element){
+    return Math.pow(element,3);
+});
+console.log(cubes);
+
+//filtering odds and evens
+let odds = numbers.filter(function filterOdd(element){
+    return element%2!==0;
+});
+console.log("Odd numbers : " + odds);
+
+let evens = numbers.filter(function(element){
+    return element%2===0;
+});
+console.log("Even Numbers : " + evens);
+
+
+//reduce method;
+
+let totalOfNumbers = numbers.reduce(function(accumulator, element){
+    return accumulator+element;
+})
+console.log("Numbers(tot) : "+ totalOfNumbers);
+
+let squareTotal = squares.reduce(function(accumulator,element){
+    return accumulator+element;
+});
+
+console.log("Sqaures(tot) : ", squareTotal);
+
+let cubesTotal = cubes.reduce(function(accumulator, element){
+    return accumulator+element;
+});
+
+console.log("Cubes(tot) : "+ cubesTotal);
+
+let oddTotal = odds.reduce(function(accumulator,element){
+    return accumulator+element;
+});
+console.log("Odds(tot) : " + oddTotal);
+
+let evenTotal = evens.reduce(function(accumulator,element){
+    return accumulator+element;
+});
+console.log("Evens(tot) : "+ evenTotal);
