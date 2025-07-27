50714b6f817bc76b3b1eb9f5e67ec9008743b8ab
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-27 19:19:49 +0530

    Objects update

--- 012/script.js
+++ 012/script.js
@@ -6,6 +6,7 @@ const person1 = {
     age:22,
     isUngergraduate :true,
     sayHello: function(){console.log(`Hello, I'm ${this.firstName}`);},
+    sayBye : function(){console.log(`Bye from ${this.firstName} ${this.secondName}`)},
 };
 
 const person2 = {
@@ -14,13 +15,16 @@ const person2 = {
     age:35,
     isUngergraduate :false,
     sayHello: function(){console.log(`Hello, I'm ${this.firstName}`);},
+    sayBye : function(){console.log(`Bye from ${this.firstName} ${this.secondName}`)},
 };
 
 console.log(`hello!, ${person1.firstName}`);
 person1.sayHello();
+person1.sayBye();
 
 console.log(`hello!, ${person2.firstName}`);
 person2.sayHello();
+person2.sayBye();
 
 for(let key in person1){
     console.log(person1[key]);
